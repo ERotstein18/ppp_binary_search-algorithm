6c588b1d93b7e00fd356ba770b46c4395181cb8d
Eyrma <erotstein@outlook,com>
2024-04-02 21:32:56 -0700

    updated

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,22 @@
 # code your iterative algorithm here
+def binary_search(data, el):
+    first = 0
+    last = len(data)-1
+    found = False
 
+    while first <= last and not found:
+        mid = (first+last) // 2
+
+        if data[mid] == el:
+            found = True
+        else:
+            if el < data[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+        
+    return found
+
+test_lst = [33, 51, 98, 101, 110, 273, 851]
+
+print(binary_search(test_lst, 101))
